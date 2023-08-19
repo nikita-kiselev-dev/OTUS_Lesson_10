6c10fd476299004959d419fdf7d41b5524adc7df
Nikita Kiselev <nikita.k@danko.team>
2023-08-19 13:26:45 +0700

    С2: Attack animation fix

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -5,104 +5,101 @@ using UnityEngine;
 
 public sealed class Player : MonoBehaviour
 {
-    private static readonly int State = Animator.StringToHash("State");
-    private const int IDLE_ANIM_STATE = 0;
-    private const int MOVE_ANIM_STATE = 1;
-    private const int ATTACK_ANIM_STATE = 2;
-    private const int DEATH_ANIM_STATE = 3;
+private static readonly int State = Animator.StringToHash("State");
+private const int IDLE_ANIM_STATE = 0;
+private const int MOVE_ANIM_STATE = 1;
+private const int ATTACK_ANIM_STATE = 2;
+private const int DEATH_ANIM_STATE = 3;
 
-    [SerializeField]
-    private float moveSpeed;
-    
-    [SerializeField]
-    private float rotationSpeed;
- 
-    [SerializeField]
-    private Animator animator;
+[SerializeField]
+private float moveSpeed;
 
-    [SerializeField] private TextMeshProUGUI m_AttackCounter;
-    [SerializeField] private TextMeshProUGUI m_DeathText;
-    
-    private Vector3 moveDirection;
-    private bool isMoving;
+[SerializeField]
+private float rotationSpeed;
 
-    private bool canAttack = true;
+[SerializeField]
+private Animator animator;
 
-    private int deathAttackCounter = 5;
-    private int currentAttackCounter = 0;
+[SerializeField] private TextMeshProUGUI m_AttackCounter;
+[SerializeField] private TextMeshProUGUI m_DeathText;
 
-    public bool CanMove = true;
-    
-    public void Move(Vector3 direction)
+private Vector3 moveDirection;
+private bool isMoving;
+
+private bool canAttack = true;
+
+private int deathAttackCounter = 5;
+private int currentAttackCounter = 0;
+
+public bool CanMove = true;
+
+public void Move(Vector3 direction)
+{
+    this.moveDirection = direction;
+    this.isMoving = true;
+}
+
+private void Update()
+{
+    m_AttackCounter.text = $"Attacks to death: {deathAttackCounter - currentAttackCounter}";
+
+    if (canAttack && Input.GetMouseButtonDown(0)&&!isMoving)
     {
-        this.moveDirection = direction;
-        this.isMoving = true;
+    Attack();
+}
+
+if (this.isMoving)
+{
+    this.UpdateMovement();
+    this.animator.SetInteger(State, MOVE_ANIM_STATE); //MOVE
+    this.isMoving = false;
+}
+else if(CanMove && canAttack)
+{
+this.animator.SetInteger(State, IDLE_ANIM_STATE); //IDLE
+}
+}
+
+private void UpdateMovement()
+{
+    var deltaTime = Time.deltaTime;
+    this.transform.position += moveDirection * this.moveSpeed * deltaTime;
+
+    var targetRotation = Quaternion.LookRotation(this.moveDirection, Vector3.up);
+    var nextRotation = Quaternion.Slerp(this.transform.rotation, targetRotation, this.rotationSpeed * deltaTime);
+    this.transform.rotation = nextRotation;
+}
+
+private void Attack()
+{
+    this.animator.Play("Attack {2}");
+
+    currentAttackCounter++;
+
+    if (currentAttackCounter >= deathAttackCounter)
+    {
+    Death();
+    Destroy(m_AttackCounter);
+    return;
     }
 
-    private void Update()
-    {
-        m_AttackCounter.text = $"Attacks to death: {deathAttackCounter - currentAttackCounter}";
-        
-        if (canAttack && Input.GetMouseButtonDown(0))
-        {
-            Attack();
-        }
-    }
+    StartCoroutine(AttackCooldown());
+}
 
-    private void FixedUpdate()
-    {
-        if (this.isMoving)
-        {
-            this.UpdateMovement();
-            this.animator.SetInteger(State, MOVE_ANIM_STATE); //MOVE
-            this.isMoving = false;
-        }
-        else if(CanMove && canAttack)
-        {
-            this.animator.SetInteger(State, IDLE_ANIM_STATE); //IDLE
-        }
-    }
+private IEnumerator AttackCooldown()
+{
+    canAttack = false;
 
-    private void UpdateMovement()
-    {
-        var deltaTime = Time.fixedDeltaTime;
-        this.transform.position += moveDirection * this.moveSpeed * deltaTime;
+    yield return new WaitForSeconds(0.4f);
 
-        var targetRotation = Quaternion.LookRotation(this.moveDirection, Vector3.up);
-        var nextRotation = Quaternion.Slerp(this.transform.rotation, targetRotation, this.rotationSpeed * deltaTime);
-        this.transform.rotation = nextRotation;
-    }
-    
-    private void Attack()
-    {
-        this.animator.Play("Attack {2}");
+    canAttack = true;
+}
 
-        currentAttackCounter++;
-
-        if (currentAttackCounter >= deathAttackCounter)
-        {
-            Death();
-            Destroy(m_AttackCounter);
-            return;
-        }
-
-        StartCoroutine(AttackCooldown());
-    }
-
-    private IEnumerator AttackCooldown()
-    {
-        canAttack = false;
-
-        yield return new WaitForSeconds(0.4f);
-
-        canAttack = true;
-    }
-
-    private void Death()
-    {
-        m_DeathText.gameObject.SetActive(true);
-        this.animator.SetInteger(State, DEATH_ANIM_STATE);
-        CanMove = false;
-        canAttack = false;
+private void Death()
+{
+    m_DeathText.gameObject.SetActive(true);
+    this.animator.SetInteger(State, DEATH_ANIM_STATE);
+    CanMove = false;
+    canAttack = false;
     }
 }
